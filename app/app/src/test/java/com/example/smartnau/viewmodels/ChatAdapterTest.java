6425aa5dc3ce9b72624f9635688841974ffb63a6
Ronny Manjang <ronny@manjang.de>
2023-06-12 10:36:58 +0200

    event listener

---
+++ app/app/src/test/java/com/example/smartnau/viewmodels/ChatAdapterTest.java
@@ -0,0 +1,18 @@
+package com.example.smartnau.viewmodels;
+
+import junit.framework.TestCase;
+
+public class ChatAdapterTest extends TestCase {
+
+    public void testOnCreateViewHolder() {
+    }
+
+    public void testGetItemViewType() {
+    }
+
+    public void testOnBindViewHolder() {
+    }
+
+    public void testGetItemCount() {
+    }
+}
