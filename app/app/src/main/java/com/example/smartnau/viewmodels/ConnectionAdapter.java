e7bfb012e602caef74ad5f2100c06e52e9c54b28
keypunk <salehiharun@gmail.com>
2023-06-12 13:26:39 +0200

    modified connection adapter

--- app/app/src/main/java/com/example/smartnau/viewmodels/ConnectionAdapter.java
+++ app/app/src/main/java/com/example/smartnau/viewmodels/ConnectionAdapter.java
@@ -6,6 +6,8 @@ import com.example.smartnau.model.MqttClient;
 public class ConnectionAdapter {
 
 
+    // Add Subscribe & Publish classes to access subscribe and publish functions
+    // MqttClient is required as an argument for both constructors
     private MqttClient mqtt;
 
     public ConnectionAdapter() {
