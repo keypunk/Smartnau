746e9f9ac042dec55beb4b009747c8f5680333a4
keypunk <salehiharun@gmail.com>
2023-06-18 16:58:30 +0200

    applied date format pattern to time values

--- app/app/src/main/java/com/example/smartnau/viewmodels/Utils.java
+++ app/app/src/main/java/com/example/smartnau/viewmodels/Utils.java
@@ -1,8 +1,18 @@
 package com.example.smartnau.viewmodels;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class Utils {
     // Calendar Klasse
-    public static String formatDateTime(long date){
-        return Long.toString(date);
+    public static String formatDateTime(long dateInMillisecond){
+        String formatPattern = "dd.MM.yy HH:mm";
+        SimpleDateFormat MillisecondToFormatPattern = new SimpleDateFormat(formatPattern,
+                Locale.GERMANY);
+
+        String datePattern = MillisecondToFormatPattern.format(new Date(dateInMillisecond));
+
+        return datePattern;
     }
 }
